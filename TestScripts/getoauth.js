8de3ffe2224ee39f189cc02ca566254fc8801c13
Nabarun <sengupta.nabarun@gmail.com>
2016-04-17 14:13:36 -0700

    Update getoauth.js

--- TestScripts/getoauth.js
+++ TestScripts/getoauth.js
@@ -2,7 +2,7 @@
  * http://usejsdoc.org/
  */
 var crmtest = require('./CrmTest/crmtest.js');
-var baseUrl ="https://na30.salesforce.com/";
+var baseUrl ="example https://na30.salesforce.com/";
 console.log('\n');
 
 //After creating a connected app in salesforce provide these info
@@ -10,11 +10,11 @@ console.log("-------------------------------------------------------------");
 console.log("-----------------Get OAuth AccessToken-----------------------")
 console.log("-------------------------------------------------------------");
 var response = crmtest.getoauth(
-		    baseUrl,
-		    "nabarun1@salesforce.com",
-			"Nabjsr123",
-			"7979963995394557396",
-			"3MVG9uudbyLbNPZNutwN5A08ykqQvi6EaM8PX0PyyR3NI0W5TFiNDhnI_TOM8Rfg9WuacsmIkApyEOTXaoyGk");
+		        baseUrl,
+		        "<<Salesforce User Name>>",
+			"<<Salesforce User Password>>",
+			"<<Salesforce Connected App Client Secret>>",
+			"<<Salesforce Connected App Client Id>>");
 console.log(response +'\n');
 var accessToken = crmtest.parseresponse(response,"access_token");
 console.log(accessToken);
