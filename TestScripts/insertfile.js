4d1cc4605b875b95eae29a7fa46bc1ec884e67b3
Nabarun <sengupta.nabarun@gmail.com>
2016-04-17 14:14:37 -0700

    Update insertfile.js

--- TestScripts/insertfile.js
+++ TestScripts/insertfile.js
@@ -2,7 +2,7 @@
  * http://usejsdoc.org/
  */
 var crmtest = require(‘./CrmTest/crmtest.js');
-var baseUrl ="https://na30.salesforce.com/";
+var baseUrl ="example https://na30.salesforce.com/";
 console.log('\n');
 
 //After creating a connected app in salesforce provide these info
@@ -10,11 +10,11 @@ console.log("-------------------------------------------------------------");
 console.log("-----------------Get OAuth AccessToken-----------------------")
 console.log("-------------------------------------------------------------");
 var response = crmtest.getoauth(
-		    baseUrl,
-		    "nabarun1@salesforce.com",
-			"Nabjsr123",
-			"7979963995394557396",
-			"3MVG9uudbyLbNPZNutwN5A08ykqQvi6EaM8PX0PyyR3NI0W5TFiNDhnI_TOM8Rfg9WuacsmIkApyEOTXaoyGk");
+		        baseUrl,
+		        "<<Salesforce User Name>>",
+			"<<Salesforce User Password>>",
+			"<<Salesforce Connected App Client Secret>>",
+			"<<Salesforce Connected App Client Id>>");
 console.log(response +'\n');
 var accessToken = crmtest.parseresponse(response,"access_token");
 console.log(accessToken);
@@ -24,7 +24,7 @@ console.log('\n');
 
 //Insert a pdf file to the chatter post
 console.log("-------------------------------------------------------------");
-console.log("------------Insert Chatter Feed With File--------------------")
+console.log("------------Insert Chatter Feed With File--------------------");
 console.log("-------------------------------------------------------------");
 response = crmtest.insertchatterfeedwithfile("<<File location>>", "<<File Name>>","<<File description>>", accessToken);
 console.log(response+'\n');
